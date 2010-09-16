fb84d280b776d948bd6a901fc48b56a67e47f309
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-09-16 09:12:38 +0000

    Fix in liveclone packaging. New icon for the Startup Guide. New color for grub2 for Salix Live.

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ VER=$(grep '^SALIX_VERSION=' src/salix-startup-guide | head -n 1 | cut -d= -f2)
 install -d -m 755 $DESTDIR/usr/bin/
 install -d -m 755 $DESTDIR/usr/doc/salix-startup-guide-$VER
 install -d -m 755 $DESTDIR/usr/share/applications/
+install -d -m 755 $DESTDIR/usr/share/icons/hicolor/128x128/apps
 install -d -m 755 $DESTDIR/install
 install -m 755 src/salix-startup-guide $DESTDIR/usr/bin/
 install -m 644 salix-startup-guide.desktop $DESTDIR/usr/share/applications/
+install -m 644 src/salixguide-logo.png $DESTDIR/usr/share/icons/hicolor/128x128/apps/salixguide-logo.png
 
 for i in `ls docs`; do
 	install -m 644 docs/${i} \
