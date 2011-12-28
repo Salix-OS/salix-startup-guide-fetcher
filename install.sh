955e9d6b69bf0d5c80ac9d92866c755b231a711e
Dimitris Tzemos <djemosATslackelDOTgr>
2011-12-28 14:35:52 +0000

    update salix-starup-guide

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ install -d -m 755 $DESTDIR/usr/share/icons/hicolor/128x128/apps
 install -d -m 755 $DESTDIR/install
 install -m 755 src/salix-startup-guide $DESTDIR/usr/bin/
 install -m 644 salix-startup-guide.desktop $DESTDIR/usr/share/applications/
+install -m 644 salix-startup-guide-kde.desktop $DESTDIR/usr/share/applications/
 install -m 644 src/salixguide-logo.png $DESTDIR/usr/share/icons/hicolor/128x128/apps/salixguide-logo.png
 
 for i in `ls docs`; do
